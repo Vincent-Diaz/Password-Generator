288b218226d864db14c1cc12547ff6cae88b3347
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-09-23 23:00:00 -0500

    added in variable inputs

--- Develop/script.js
+++ Develop/script.js
@@ -19,5 +19,11 @@ var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "/", "<", "
 var aUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var aLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Variable Inputs
+var confirmLength = "";
+var confirmNumericCharacter;
+var confirmSpecialCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
 
