16e67b1730852577bd023af4dbdd1ebbbfa52322
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-09-23 22:59:17 -0500

    added in arrays

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Various Arrays
+var number = Math.floor(Math.random() * 10) + 1;
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "/", "<", ">", "~", "?", "."];
+var aUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var aLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+
